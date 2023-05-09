05019640a2ff38f2bc324e8f946619263db7702d
Pascal Pfannes <ppfannes@students.uni-mainz.de>
2023-05-09 14:47:07 +0200

    Try to fix Mario collision; Some small fixes.

--- physics2d/components/circle_collider.py
+++ physics2d/components/circle_collider.py
@@ -1,5 +1,6 @@
 import glm
 from components.component import Component
+from physics2d.components.rigid_body_2d import RigidBody2D
 from renderer.debug_draw import DebugDraw
 
 
@@ -7,6 +8,7 @@ class CircleCollider(Component):
     def __init__(self):
         super().__init__()
         self._radius = 1.0
+        self._reset_fixture_next_frame = False
         self._offset = glm.fvec2(0.0)
 
     @property
@@ -15,6 +17,7 @@ class CircleCollider(Component):
 
     @radius.setter
     def radius(self, value):
+        self._reset_fixture_next_frame = True
         self._radius = value
 
     @property
@@ -28,3 +31,34 @@ class CircleCollider(Component):
     def editor_update(self, dt):
         center = glm.add(self.game_object.transform.position, self.offset)
         DebugDraw.add_circle(center, self._radius)
+
+        if self._reset_fixture_next_frame:
+            self.reset_fixture()
+
+    def update(self, dt):
+        if self._reset_fixture_next_frame:
+            self.reset_fixture()
+
+    def reset_fixture(self):
+        from metroid_maker.window import Window
+
+        if Window.get_physics().is_locked():
+            self._reset_fixture_next_frame = True
+            return
+
+        self._reset_fixture_next_frame = False
+
+        if self.game_object is not None:
+            rigid_body = self.game_object.get_component(RigidBody2D)
+
+            if rigid_body is not None:
+                Window.get_physics().reset_circle_collider(rigid_body, self)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["_reset_fixture_next_frame"]
+        return state
+
+    def __setstate__(self, state):
+        state["_reset_fixture_next_frame"] = False
+        self.__dict__.update(state)
